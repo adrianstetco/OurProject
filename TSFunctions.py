b800ee1e164c3e093c0a73ffffb9e961104f7b6f
adrianstetco <adrian.stetco@yahoo.com>
2016-10-12 14:03:24 +0100

    Update TSFunctions.py

--- TSFunctions.py
+++ TSFunctions.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib
 import math
 import talib as tb
+from talib import abstract
+from talib.abstract import *
 matplotlib.style.use('ggplot')
 
 np.set_printoptions(threshold=np.nan)
@@ -14,7 +16,7 @@ def readts(path, norm="FALSE"):
                          names=['date', 'time', 'value0', 'value1', 'value2', 'value3'])
     df = df.iloc[:, [0, 3]].as_matrix()
     ts = pd.Series(df[:, 1], index=df[:, 0])
-    #if norm == "TRUE": df[:, 1] = df[:, 1] / np.linalg.norm(df[:, 1])
+    #if norm == "TRUE": df[:, 1] = df[:, 1] / np.linalg.norm(df[:, 1]) not yet
     return ts
 
 # SPLITS NUMPY ARRAY INTO TRAINING AND TESTING SETS
@@ -30,20 +32,25 @@ def generate(dataset, n, timestamps="FALSE"):
     dataset= np.reshape(dataset,(-1,(n+1) *2 ))
     print(dataset)
 
+# visualize price time series together with indicator in the same plot
+def visualize(ts, TALibFuncName):
+  inputs = {'close': np.array(ts.values,dtype='f8')}
+  TALibFunction= abstract.Function(TALibFuncName[0])
+  TALibResult= TALibFunction(inputs,60) #parameter for SMA
+  concat= np.column_stack([ts.values, TALibResult])
+  df = pd.DataFrame(concat, index=ts.index, columns= ['Price',TALibFuncName[0]])
+  df.plot(); plt.show()
 
-ts = readts('C:\Users\Adrian\Desktop\EURAUDSmall.txt', norm="TRUE")
+
+
 #converted = ts.asfreq('60Min', method='pad')
-
-#print(ts.values)
-SMA= tb.SMA(np.array(ts.values,dtype='f8'))
-pds =pd.Series(SMA)
-#print(len(ts))
-#print(len(SMA))
-#print(np.randn(1000, 4))
-#df = pd.DataFrame([ts.values,SMA], index=ts.index, columns=list('AB'))
-pds.plot(legend=True);
-plt.show()
+#ts=ts.ix[1:50]
 #train, test = split(s, 0.6)
 #generate(train,5)
+#print(ts.values)
+#print(SMA)
 
-
+TALibFuncNames = np.array(pd.read_csv('C:\Users\Adrian\Desktop\TA-list.txt'))
+ts = readts('C:\Users\Adrian\Desktop\EURAUDSmall.txt', norm="TRUE")
+ts=ts.ix[1:500]
+visualize(ts, TALibFuncNames[1])
