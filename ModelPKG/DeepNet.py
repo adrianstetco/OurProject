05bf3ba81fe794ebecebc4c57d516f39bbf3d18c
adrianstetco <adrian.stetco@yahoo.com>
2016-10-14 12:21:16 -0400

    Update DeepNet.py

--- ModelPKG/DeepNet.py
+++ ModelPKG/DeepNet.py
@@ -1,6 +1,28 @@
 from BaseModel import abstractmodel
+import numpy as np
+import tflearn
 
 class model(abstractmodel):
-    def train(self): print("Deep Net train")
-    def test(self): print("Deep Net test")
+    def train(self, data): 
+        print("Deep Net train")
+        net = tflearn.input_data(shape=[None, 6])
+        net = tflearn.fully_connected(net, 32)
+        net = tflearn.fully_connected(net, 32)
+        net = tflearn.fully_connected(net, 2, activation='softmax')
+        net = tflearn.regression(net)
+        # Define model
+        model = tflearn.DNN(net)
+        # Start training (apply gradient descent algorithm)
+        examples=data[:,0:data.shape[1]-2]
+        print(examples.shape)
+        labels=data[:,data.shape[1]-1]
+        print(labels)
+        print(type(examples))
+        print(type(labels))
+        labels = tflearn.data_utils.to_categorical(labels, 3)
+        model.fit(examples, labels , n_epoch=10, batch_size=16, show_metric=True)
+        return model
+    
+    def test(self):
+        print("Deep Net test")
     
